70bf2dd14251e82dd29e9fc89f814252b3598a61
nathan3k <n4th4n3k@gmail.com>
2020-01-08 14:57:43 +0100

    added some code idk lol xddxdx

--- bot.js
+++ bot.js
@@ -1,1 +1,21 @@
-d
+//discord bot application
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const {token, prefix}=require('./config.json');
+
+const users = 
+
+client.login(token);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content.startsWith(`${prefix}alakazam`)) {
+    msg.reply('Pong!');
+    const onlineUsers = client.users;
+    console.log(client.guilds);
+    // users.forEach(user => { })
+  }
+});
