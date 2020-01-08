3c2b10a627e71f3f42ecf4ba442b5e2fe94e6e2f
Marcin Ptok <marcin111100@gmail.com>
2020-01-08 17:27:37 +0100

    added change nickname function

--- bot.js
+++ bot.js
@@ -1,9 +1,22 @@
 //discord bot application
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const {token, prefix}=require('./config.json');
+const {
+  token,
+  prefix
+} = require('./config.json');
 
-const users = 
+const users = []
+const availableNicknames = []
+const formatUser = userData => {
+  const user = {
+    "id": userData.id,
+    "username": userData.username,
+    "discriminator": userData.discriminator
+  }
+  users.push(user);
+  availableNicknames.push(userData.username)
+}
 
 client.login(token);
 
@@ -15,7 +28,23 @@ client.on('message', msg => {
   if (msg.content.startsWith(`${prefix}alakazam`)) {
     msg.reply('Pong!');
     const onlineUsers = client.users;
-    console.log(client.guilds);
-    // users.forEach(user => { })
+    onlineUsers.forEach(user => {
+      formatUser(user)
+    })
+    users.forEach(user => {
+      const tempAvailableNicknames = availableNicknames.filter(val => val != user.username);
+      msg.guild.fetchMember(user.id)
+        .then(member => {
+          member.setNickname(tempAvailableNicknames[Math.floor(Math.random() * tempAvailableNicknames.length)]).catch(e => console.log(e))
+        })
+    })
+
+  } else if (msg.content.startsWith(`${prefix}info`)) {
+    const serverIcon = msg.guild.iconURL;
+    const embed = new Discord.RichEmbed()
+      .setDescription("Informacje o serwerze")
+      .setThumbnail(serverIcon)
+      .addField("Ilość członków: ", msg.guild.memberCount);
+    msg.channel.send(embed);
   }
 });
